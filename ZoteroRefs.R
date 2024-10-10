3f1ff6bc42e1aa4d871f2afb5555e94c3924e4c3
Corey Bradshaw <cjabradshaw@gmail.com>
2024-10-10 11:51:08 +0200

    Add files via upload

---
+++ ZoteroRefs.R
@@ -0,0 +1,23 @@
+## Access a specific subset of your local Zotero database
+## (ensure Zotero desktop app is closed or code will not be able to access the database)
+
+remotes::install_github("FRBCesab/zoteror")
+library(zoteror)
+
+zotlib <- get_zotero_data(path = "~/Zotero")
+str(zotlib)
+head(zotlib)
+
+table(zotlib$group)
+
+InvaHzot <- subset(zotlib, group == 'InvaHealth')
+head(InvaHzot)
+table(InvaHzot$collection)
+
+InvaHDBzot <- subset(InvaHzot, collection == "Papers database")
+dim(InvaHDBzot)
+str(InvaHDBzot)
+
+## article DOIs
+InvaHDBzot$doi
+
